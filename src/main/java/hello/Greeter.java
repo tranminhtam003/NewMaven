6617fcb32c7741ae9c841caebd10521bfe706092
tranminhtam003 <tmtam@tma.com.vn>
2017-12-22 16:48:31 +0700

    first commit

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,10 @@
+package main.java.hello;
+
+/**
+ * Created by tmtam on 12/22/2017.
+ */
+public class Greeter {
+    public String sayHello(String text) {
+        return "Hello world!" + text;
+    }
+}
---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package main.java.hello;
+
+/**
+ * Created by tmtam on 12/22/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello("Tam"));
+    }
+}
